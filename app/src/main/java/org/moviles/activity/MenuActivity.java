7ff77594f0d4b97879a0f3643795eed43c886376
Walter <wperez790@alumnos.iua.edu.ar>
2019-09-15 18:32:40 -0300

    Drawer incomplete

--- app/src/main/java/org/moviles/activity/MenuActivity.java
+++ app/src/main/java/org/moviles/activity/MenuActivity.java
@@ -2,17 +2,24 @@ package org.moviles.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 
+
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import java.io.File;
 
 public class MenuActivity extends AppCompatActivity {
 
     private Button btnCerrarUsuario;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +35,24 @@ public class MenuActivity extends AppCompatActivity {
         });
 
         setTitle("Menu");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer, toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
     }
 
     private void cerrarSesion(){
